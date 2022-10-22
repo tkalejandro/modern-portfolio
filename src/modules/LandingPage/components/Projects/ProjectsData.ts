328e29c4a38e502011d6c010d98a574c0f7d1830
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-10-22 16:11:05 +0200

    project layout reworked

---
+++ src/modules/LandingPage/components/Projects/ProjectsData.ts
@@ -0,0 +1,16 @@
+import { ProjectInfo } from "../../../../types/LandingPage"
+
+const projectData : ProjectInfo[] = [{
+    name: '100Best Mobile',
+    image: {
+        src: '',
+        alt:''
+    },
+    description: '',
+    tools:'',
+    github: undefined,
+    gitlab: undefined,
+    teamWork: undefined
+}]
+
+export default projectData
