deeb8ac1cc11f69b260005d1432bb6d630ce3775
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-10-22 16:07:20 +0200

    project info added and images.

---
+++ src/types/LandingPage/ProjectInfo.ts
@@ -0,0 +1,32 @@
+type ProjectInfo = {
+    /**
+     * Name of the project
+     */
+    name: string,
+    /**
+     * Image local source or url and Alt text.
+     */
+    image: {src: string, alt: string},
+    /**
+     * Description of the project
+     */
+    description: string,
+    /**
+     * Laguages and tools used for the project
+     */
+    tools:string,
+    /**
+     * GitLab url or undefined.
+     */
+    gitlab?: string,
+    /**
+     * GitHub url or undefined
+     */
+    github?: string,
+    /**
+     * Team or partner url portfolio / website or undefined
+     */
+    teamWork?:string
+}
+
+export default ProjectInfo
--- src/types/LandingPage/index.ts
+++ src/types/LandingPage/index.ts
@@ -1,2 +1,3 @@
 export type { default as FullStackToolsInfo} from './FullStackToolsInfo'
 export type { default as OtherToolsInfo} from './OtherToolsInfo'
+export type { default as ProjectInfo} from './ProjectInfo'
