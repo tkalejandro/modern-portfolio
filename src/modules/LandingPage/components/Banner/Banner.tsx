7d398b246658f8d059e33aa68aefbcf2a5ed890f
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-10-31 10:44:05 +0100

    First abnner additon.

--- src/modules/LandingPage/components/Banner/Banner.tsx
+++ src/modules/LandingPage/components/Banner/Banner.tsx
@@ -1,20 +1,48 @@
-import { Box, Grid, Paper, styled } from '@mui/material';
+import { Box, Container, Grid, Paper, styled, Typography } from '@mui/material';
 import React from 'react';
 import { LandingPageIds } from '../../../../constants';
 
-
+/**
+ * Principal Banner. Quick information of my skills
+ * @returns 
+ */
 const Banner = () => {
 
+    const data: string[] = ['MongoDb', 'Express', 'SQL', 'ASP.NET', 'React', 'Javascript', 'Typescript', 'React Native']
+
     return (
-        <Box 
-        id={LandingPageIds.home}
-        sx={(theme) =>({
-            background: theme.palette.primary.main,
-            border: "2px solid green",
-            width: "100%",
-            height: "100vh"
-        })}>
-            Banner
+        <Box
+            id={LandingPageIds.home}
+            sx={(theme) => ({
+                background: theme.palette.background.default,
+                border: "2px solid green",
+                width: "100%",
+                minHeight: "100vh",
+                height: "100vh"
+            })}>
+            <Container sx={{ py:10, height: '100%', display: 'flex', justifyContent: 'center', flexDirection:'column'}}>
+                <Typography variant="h3">{"<Alejandro!>"}</Typography>
+                <Typography variant="h4" fontWeight={700} color="secondary">Full-Stack Web-Developer</Typography>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'center' }}>
+                    {
+                        data.map((str) => {
+                            return <Typography 
+                            variant='body2'
+                            sx={{
+                                my: 0.5,
+                                mr: 1,
+                                px: 1,
+                                py: 0.5,
+                                border: 1,
+                                borderRadius: '12px'
+                                
+                            }}
+                            my={0.5} mr={1} p={0.5} key={str}>{str}</Typography>
+                        })
+                    }
+
+                </Box>
+            </Container>
         </Box>
     );
 };
